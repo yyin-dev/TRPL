1bc367c78939ea6bcd07351c21e4f8322481ec75
Yin Yue <yinyue.fred@bytedance.com>
2021-03-17 21:45:05 +0800

    Chapter 3

---
+++ ch3/functions/src/main.rs
@@ -0,0 +1,58 @@
+fn main() {
+    // Difference between statements and expressions in Rust:
+    // Statements are instructions that perform some action and do not return 
+    // a value. Expressions evaluate to a resulting value. 
+
+    // Making a function call is an expression; a numeric literal is an
+    // expression; {} is an expression.
+    let y = {
+        let x = 3;
+        x + 1 
+        // Expressions don't include ending semicolons. Adding a semicolon
+        // to the end of an expression turns it into a statement.
+    };
+    println!("{}", y);
+
+    // Expression as return value example
+    println!("Five: {}", return_five());
+
+    // using `if` in a `let` statement
+    let number = if true { 5 } else { 6 };
+    println!("{}", number);
+
+    // Return values from loop: add the value after the `break` keyword
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+    println!("The result is {}", result);
+
+    // for loop 
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+
+    // Exercise
+    println!("Fib(10) = {}", fib(10));
+}
+
+fn return_five() -> i32 {
+    // The last expression is implicitly the return value
+    5
+}
+
+fn fib(n: i32) -> i32{
+    if n <= 1 {
+        return 1;
+    }
+    fib(n-1) + fib(n-2)
+}
---
+++ ch3/variables/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    // Variables are immutable by default. But it's different from constants.
+    // You cannot use `mut` on constants;
+    // Variables are declared with `let`, while constants are declared with 
+    // `const` and must be type annotated;
+    // Constants can only be set to constant expressions.
+
+    // Shadowing
+    let mut x = 5;
+    x += 1;
+    println!("{}", x);
+
+    let x = 6;
+    println!("{}", x);
+
+    // Rust is a statically typed language, so all types must be known at 
+    // compile time. The compiler will try to infer, but when it cannot, 
+    // the type must be annotated. Example: parse().
+
+    // Scalar types: integer, floating-point numbers, boolean, character
+    // Compound types: tuple, array
+
+    // Tuple type
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup; // destructuring
+    println!("The tuple is ({}, {}, {}), OR ({}, {}, {})", x, y, z, tup.0, tup.1, tup.2);
+
+    // Array type
+    // Array in Rust has fixed length.
+    // Vector is provided by the standard library that can grow and shrink.
+    // An array is a single chunk of memory allocated on the stack.
+    // Accessing past the end of an array is a runtime error.
+    let a : [i32; 4] = [0, 1, 2, 3];
+    println!("{:?}", a);
+    let a = [1024; 5];
+    println!("{:?}", a);
+    println!("{}", a[0]);
+}
