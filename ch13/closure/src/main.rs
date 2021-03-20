9bdfdb2ce96f9403c310e5a520c3f3259b7a3495
Yin Yue <yinyue.fred@bytedance.com>
2021-03-20 12:47:47 +0800

    Finish Chapter 13

---
+++ ch13/closure/src/main.rs
@@ -0,0 +1,106 @@
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::thread;
+use std::time::Duration;
+
+struct Cacher<T, U, V>
+where
+    T: Hash + Eq + Clone,
+    U: Clone,
+    V: Fn(T) -> U,
+{
+    // All closures implement at least one of the traits: Fn, FnMut, FnOnce.
+    calculation: V,
+    values: HashMap<T, U>,
+}
+
+impl<T, U, V> Cacher<T, U, V>
+where
+    T: Hash + Eq + Clone,
+    U: Clone,
+    V: Fn(T) -> U,
+{
+    fn new(calculation: V) -> Cacher<T, U, V> {
+        Cacher {
+            calculation,
+            values: HashMap::new(),
+        }
+    }
+
+    fn value(&mut self, args: T) -> U {
+        match self.values.get(&args) {
+            Some(val) => val.clone(),
+            None => {
+                let key = (&args).clone();
+                let res = (self.calculation)(args);
+                let v = res.clone();
+                self.values.insert(key, v); // fn insert(&mut self, k: K, v: V) -> Option<V>
+                res
+            }
+        }
+    }
+}
+
+fn generate_workout(intensity: u32, random_number: u32) {
+    // Closure doesn't require type annotations for parameters and return
+    // values. Functions require type annotations because they are part of an
+    // explicit interface. However, closures aren't used in an exposed
+    // interface. They are stored in variables and without being exposed.
+    // Thus, the compiler is able to infer the types of parameterse and return
+    // value.
+    let mut expensive_result = Cacher::new(|num| {
+        println!("calculating slowly...");
+        thread::sleep(Duration::from_secs(2));
+        num
+    });
+
+    if intensity < 25 {
+        println!("Today, do {} pushups!", expensive_result.value(intensity));
+        println!("Next, do {} situps!", expensive_result.value(intensity));
+    } else {
+        if random_number == 3 {
+            println!("Take a break today! Remember to stay hydrated!");
+        } else {
+            println!(
+                "Today, run for {} minutes!",
+                expensive_result.value(intensity)
+            );
+        }
+    }
+}
+
+fn main() {
+    let simulated_user_specified_value = 10;
+    let simulated_random_number = 7;
+    generate_workout(simulated_user_specified_value, simulated_random_number);
+
+    // Closures can capture environment. This is implemented by storing the 
+    // values for use in the closure body. This is extra overhead.
+    // Closures can capture values from the environment in three ways: taking
+    // ownership (FnOnce), borrowing mutably (FnMut), borrowing immutably (Fn).
+    //
+    // When you create a closre, Rust infers which trait to use based on how 
+    // the closure uses the values from the environment. 
+    // If you want to force the closre to take ownership of the values it uses
+    // in the environment, you can use `move` keyword before the parameter list.
+    // 
+    let x = 4;
+    let equal_to_x = |z| z == x;
+    let y = 4;
+    assert!(equal_to_x(y));
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn call_with_different_values() {
+        let mut c = Cacher::new(|a| a);
+
+        let v1 = c.value(1);
+        let v2 = c.value(2);
+
+        assert_eq!(v2, 2);
+    }
+}
---
+++ ch13/iterators/src/main.rs
@@ -0,0 +1,66 @@
+#![allow(unused)]
+
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
+fn main() {
+    pub trait Iterator {
+        // `type Item` defines an associated type with the trait. This means
+        // that `Item` will be the return type from the iterator.
+        type Item;
+
+        fn next(&mut self) -> Option<Self::Item>;
+
+        // methods with default implementations elided
+    }
+    
+    // Rust compiler optimizes iterator very well: zero-cost abstraction.
+    // "clearly express high-level ideas at low-level performance"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn iterator_demonstration() {
+        let v1 = vec![1, 2, 3];
+
+        let mut v1_iter = v1.iter();
+
+        assert_eq!(v1_iter.next(), Some(&1));
+        assert_eq!(v1_iter.next(), Some(&2));
+        assert_eq!(v1_iter.next(), Some(&3));
+        assert_eq!(v1_iter.next(), None);
+    }
+
+    #[test]
+    fn using_other_iterator_trait_methods() {
+        let sum: u32 = Counter::new() // 1, 2, 3, 4, 5
+            .zip(Counter::new().skip(1)) // (1,2), (2,3), (3,4), (4,5)
+            .map(|(a, b)| a * b) // 2, 6, 12, 20
+            .filter(|x| x % 3 == 0)
+            .sum();
+        assert_eq!(18, sum);
+    }
+}
